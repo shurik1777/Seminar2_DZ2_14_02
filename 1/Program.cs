a98db4bd7ccc475a0a9375259ab0f58ea5070cad
Aleksandr_Wosmerik <shurik1777@gmail.com>
2023-02-16 21:50:29 +0300

    first work done

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,3 +4,18 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    int second = FindSecondDigit(number);
+    Console.WriteLine($"Вторая цифра трехзначного числа -> {second}");
+}
+else Console.WriteLine("Введено не трехзначное число!");
+// Метод для поиска 2й цифры трехзначного числа:
+int FindSecondDigit(int number)
+{
+    return (number / 10 % 10);
+}
